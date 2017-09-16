258052db9de03550ec3984201651afee77142e4f
abdo1819 <abdo14hashem15@gmail.com>
2017-09-16 19:38:37 +0200

    update_01
    reorgnize

--- file & codes/01.py
+++ file & codes/01.py
@@ -1,18 +1,18 @@
-import time
-start = time.time()
+
 
 from sklearn import tree
-import graphviz
+
 #make data
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
-#classidier
+#classifier
 clf = tree.DecisionTreeClassifier()
 clf.fit(features,labels)
 
 print (clf.predict([[145,1]]))
 
 #view in graph
+import graphviz
 dot_data = tree.export_graphviz(clf,out_file = None
                                 ,feature_names=["wieght","smothy"],
                                 class_names=["apple","orange"],
@@ -21,7 +21,3 @@ dot_data = tree.export_graphviz(clf,out_file = None
 
 graph = graphviz.Source(dot_data)
 graph.render("iris")
-
-
-
-print("time = ",time.time()-start)
